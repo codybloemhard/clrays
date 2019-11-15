8a2bc709dd423574a4c752c91d8182ddca727a0c
Cody Bloemhard <cblmhrd@live.nl>
2019-11-15 17:27:53 +0100

    test audio

--- clrays-rs/src/lib.rs
+++ clrays-rs/src/lib.rs
@@ -13,6 +13,10 @@ use sdl2::keyboard::Keycode;
 use std::time::Duration;
 
 pub fn run(){
+    test_audio();
+}
+
+pub fn test_window(){
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
@@ -46,4 +50,54 @@ pub fn run(){
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+}
+
+pub fn test_audio(){
+    use sdl2::audio::{AudioCallback, AudioSpecDesired};
+
+    struct SquareWave {
+        phase_inc: f32,
+        phase: f32,
+        volume: f32
+    }
+    
+    impl AudioCallback for SquareWave {
+        type Channel = f32;
+    
+        fn callback(&mut self, out: &mut [f32]) {
+            // Generate a square wave
+            for x in out.iter_mut() {
+                *x = if self.phase <= 0.5 {
+                    self.volume
+                } else {
+                    -self.volume
+                };
+                self.phase = (self.phase + self.phase_inc) % 1.0;
+            }
+        }
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1),  // mono
+        samples: None       // default sample size
+    };
+
+    let device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        // initialize the audio callback
+        SquareWave {
+            phase_inc: 440.0 / spec.freq as f32,
+            phase: 0.0,
+            volume: 0.25
+        }
+    }).unwrap();
+
+    // Start playback
+    device.resume();
+
+    // Play for 2 seconds
+    std::thread::sleep(Duration::from_millis(2000));
 }
