23e07be9810e60d7224c8a1b77be5f81e57d4041
Cody Bloemhard <cblmhrd@live.nl>
2019-11-18 16:35:37 +0100

    image kernel and result kernel stuff

--- clrays-rs/src/kernels.rs
+++ clrays-rs/src/kernels.rs
@@ -4,27 +4,23 @@ use std::rc::Rc;
 
 pub trait VoidKernel<T: ocl::OclPrm>{
     fn execute(&mut self, queue: &Queue) -> Result<(), ocl::Error>;
-    fn get_buffer(&self) -> Rc<ClBuffer<T>>;
+    fn get_buffer(&self) -> &ClBuffer<T>;
 }
 
 pub trait ResultKernel<T: ocl::OclPrm>{
-    fn execute();
-    fn get_buffer() -> Buffer<T>;
-    fn get_result() -> Vec<T>;
+    fn get_result(&mut self, queue: &Queue) -> Result<&[T],ocl::Error>;
 }
 
 pub struct ClearKernel{
     kernel: Kernel,
     buffer: Rc<ClBuffer<f32>>,
-    work: (u32,u32),
 }
 
 impl ClearKernel{
-    pub fn new(name: String, res:(u32,u32), program: &Program, queue: &Queue, buffer: Rc<ClBuffer::<f32>>) -> Result<Self,ocl::Error>{
-        let (w,h) = res;
+    pub fn new(name: &str, (w,h): (u32,u32), program: &Program, queue: &Queue, buffer: Rc<ClBuffer<f32>>) -> Result<Self,ocl::Error>{
         let kernel = match Kernel::builder()
         .program(program)
-        .name(&name)
+        .name(name)
         .queue(queue.clone())
         .global_work_size([w,h])
         .arg(buffer.get_ocl_buffer())
@@ -36,7 +32,6 @@ impl ClearKernel{
         Result::Ok(Self{
             kernel,
             buffer,
-            work: res,
         })
     }
 }
@@ -51,7 +46,71 @@ impl VoidKernel<f32> for ClearKernel{
         }
     }
 
-    fn get_buffer(&self) -> Rc<ClBuffer<f32>>{
-        self.buffer.clone()
+    fn get_buffer(&self) -> &ClBuffer<f32>{
+        &self.buffer
     }
 }
+
+pub struct ImageKernel{
+    kernel: Kernel,
+    buffer: ClBuffer<i32>,
+    dirty: bool,
+}
+
+impl ImageKernel{
+    pub fn new(name: &str, (w,h): (usize,usize), program: &Program, queue: &Queue, input: &ClBuffer<f32>) -> Result<Self,ocl::Error>{
+        let dirty = false;
+        let buffer = match ClBuffer::<i32>::new(queue, w * h, 0){
+            Ok(x) => x,
+            Err(e) => return Err(e),
+        };
+        let kernel = match Kernel::builder()
+        .program(program)
+        .name(name)
+        .queue(queue.clone())
+        .global_work_size([w,h])
+        .arg(input.get_ocl_buffer())
+        .arg(buffer.get_ocl_buffer())
+        .arg(w as u32)
+        .arg(h as u32)
+        .build(){
+            Ok(x) => x,
+            Err(e) => return Err(e),
+        };
+        Ok(Self{
+            buffer,
+            kernel,
+            dirty,
+        })
+    }
+}
+
+impl VoidKernel<i32> for ImageKernel{
+    fn execute(&mut self, queue: &Queue) -> Result<(), ocl::Error>{
+        unsafe { 
+            match self.kernel.cmd().queue(queue).enq(){
+                Ok(_) => { self.dirty = true; Ok(()) },
+                Err(e) => Err(e),
+            }
+        }
+    }
+
+    fn get_buffer(&self) -> &ClBuffer<i32>{
+        &self.buffer
+    }
+}
+
+impl ResultKernel<i32> for ImageKernel{
+    fn get_result(&mut self, queue: &Queue) -> Result<&[i32],ocl::Error>{
+        if self.dirty {
+            match self.buffer.download(queue){
+                Ok(_) => {},
+                Err(e) => return Err(e),
+            }
+        }
+        self.dirty = false;
+        Ok(self.buffer.get_slice())
+    }
+}
+
+
